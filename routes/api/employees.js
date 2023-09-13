3a73548578d85298e7454afbda115aec53d658dd
JavDaGreat <98164907+JavDaGreat@users.noreply.github.com>
2023-09-13 18:44:35 +0200

    Update

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,6 +1,6 @@
 const express= require("express")
 const router=express.Router()
-const employeeController= require("../../controller/employeesController")
+const employeeController= require("../../controllers/employeesController")
 
 
 router.route("/")
